38d6fcfc810355eac4a2c3a8288ce75989a033b8
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-05-27 11:53:05 +0200

    removed extra props no needed

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import CharacterCard from '../CharacterCard/CharacterCard';
-import './CharacterList.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import CharacterCard from "../CharacterCard/CharacterCard";
+import "./CharacterList.scss";
 
 const List = props => {
-    return (
-        <ul className = "list">
-        {props.people.map(character => (
-          <li key={character.name} className = "list__item">
-            <CharacterCard
-                    name = {character.name}
-                    img = {character.image}
-                    house = {character.house}
-                />
-                <div className = "link__container">
-                <Link to={`/character-detail/${character.id}`} 
-                className = "link">
-                  More about {character.name}
-                </Link>
-                </div>
-          </li>
-        ))}
-      </ul>
-    )
-}
+  return (
+    <ul className="list">
+      {props.people.map(character => (
+        <li key={character.name} className="list__item">
+          <CharacterCard
+            name={character.name}
+            img={character.image}
+            house={character.house}
+          />
+          <div className="link__container">
+            <Link to={`/character-detail/${character.id}`} className="link">
+              More about {character.name}
+            </Link>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 List.propTypes = {
-    people: PropTypes.arrayOf(PropTypes.object).isRequired
-}
+  people: PropTypes.arrayOf(PropTypes.object).isRequired
+};
 
 export default List;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,25 @@
-import React from 'react';
-import Filters from '../Filters/Filters';
-import CharacterList from '../CharacterList/CharacterList';
+import React from "react";
+import Filters from "../Filters/Filters";
+import CharacterList from "../CharacterList/CharacterList";
 
 const Home = props => {
+  const { people, name, onSearch } = props;
 
-    const {people, name, onSearch} = props;
+  return (
+    <div>
+      <header className="title">
+        <h1>"Harry Potter Characters"</h1>
+      </header>
 
-    return (
-       <div>
-        <header className = "title">
-          <h1>"Harry Potter Characters"</h1>
-        </header>
+      <Filters onSearch={onSearch} name={name} />
 
-        <Filters 
-            onSearch ={onSearch}
-            name={name}
-        />
-        
-        <CharacterList 
-            people={people.filter(character => 
-            character.name.toUpperCase().includes(name.toUpperCase()))}
-            name={name}
-            onSearch={onSearch}
-        />
-        </div>
-    )
-}
+      <CharacterList
+        people={people.filter(character =>
+          character.name.toUpperCase().includes(name.toUpperCase())
+        )}
+      />
+    </div>
+  );
+};
 
 export default Home;
